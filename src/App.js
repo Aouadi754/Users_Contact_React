1f591e2a8020655a082564eefd287be5ed2640c3
wassim mokhtar <wassimmokhtar92@gmail.com>
2020-10-22 16:38:25 +0100

    button and link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-
+     <Link to='/contact list'><Button variant="primary">contact list</Button> </Link> 
+     <Link to='/add contact'> <Button variant="primary">add contact</Button></Link>
     </div>
   );
 }
