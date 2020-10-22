506aa5f4baa8d5e9b5842e58d59713c6b6c3cb28
wassim mokhtar <wassimmokhtar92@gmail.com>
2020-10-22 17:06:14 +0100

    Add Component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import {Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-
+import Add from './Add';
 
 function App() {
   return (
+    <div className='List'>
     <div className="App">
      <Link to='/contact list'><Button variant="primary">contact list</Button> </Link> 
      <Link to='/add contact'> <Button variant="primary">add contact</Button></Link>
+     </div>
+       <Add/>
     </div>
   );
 }
